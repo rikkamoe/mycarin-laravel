1822b2aa0c00fb06ffa775020101a9b912d63e4b
zipperiz <arizmenggila@gmail.com>
2022-01-21 16:18:00 +0800

    editan terakhir

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-6 mb-5">
             <h1 class="mt-5">MyCarin</h1>
-            <h4 class="mt-5">Mau setir sendiri atau dengan supir bisa dilakukan dalam satu aplikasi yang dilengkapi 
-                dengan referensi kendaraan sesuai prediksi cuaca demi kenyamanan anda.</h4>
+            <h4 class="mt-4">Setir Mobil sendiri atau menggunakan supir dengan satu aplikasi yang dilengkapi 
+                dengan rekomendasi kendaraan sesuai prediksi cuaca untuk kenyamanan anda.</h4>
         </div>
         <div class="col-md-6 mb-5">
             <img src="{{ url('img/hero-img.svg') }}" width="500px" class="rounded mx-auto d-block img-fluid" alt="..."/>
         </div>
         <div class="row mt-3">
             <div class="col-md-10">
-                <h3 class="mb-3"><b>Produk Rekomendasi Cuaca Hari ini </b></h3>
+                <h3 class="mb-3"><b>Mobil Rekomendasi Cuaca Hari ini </b></h3>
             </div>
             <div class="col-md-2">
                 <img src="https://img.icons8.com/color/50/000000/rain--v1.png"/> 18&#8451; Denpasar
